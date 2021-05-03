6ff03b8b7181261fa41a3af9d9033f446ba77721
hgs-study <hgstudy_@naver.com>
2021-05-03 19:49:53 +0900

    Batch & Scheduler 초기설정

---
+++ src/main/java/com/batchschedulerbasic/BatchSchedulerBasicApplication.java
@@ -0,0 +1,13 @@
+package com.batchschedulerbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchSchedulerBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchSchedulerBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/batchschedulerbasic/BatchSchedulerBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batchschedulerbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchSchedulerBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
