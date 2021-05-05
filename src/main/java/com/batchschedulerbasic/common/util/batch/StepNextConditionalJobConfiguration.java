7c1082fb62a3cef5b3535d4078d819f7d08b5930
hgs-study <hgstudy_@naver.com>
2021-05-05 15:38:17 +0900

    [Batch] 배치 성공시 시나리오(step1 -> step2 -> step3)

--- src/main/java/com/batchschedulerbasic/common/util/batch/StepNextConditionalJobConfiguration.java
+++ src/main/java/com/batchschedulerbasic/common/util/batch/StepNextConditionalJobConfiguration.java
@@ -2,6 +2,7 @@ package com.batchschedulerbasic.common.util.batch;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
