eb528253dfcf8e196b92bf71028dc7e6e7107193
hgs-study <hgstudy_@naver.com>
2021-05-07 00:03:44 +0900

    [Batch] ItemProcessor

---
+++ src/main/java/com/batchschedulerbasic/common/util/batch/ItemProcessor/ProcessorConvertJobConfiguration.java
@@ -0,0 +1,79 @@
+package com.batchschedulerbasic.common.util.batch.ItemProcessor;
+
+import com.batchschedulerbasic.common.util.batch.Pay;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class ProcessorConvertJobConfiguration {
+
+    public static final String JOB_NAME = "ProcessorConvertBatch";
+    public static final String BEAN_PREFIX = JOB_NAME + "_";
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory emf;
+
+    @Value("${chunkSize:1000}")
+    private int chunkSize;
+
+    @Bean(JOB_NAME)
+    public Job job() {
+        return jobBuilderFactory.get(JOB_NAME)
+                .preventRestart()
+                .start(step())
+                .build();
+    }
+
+    @Bean(BEAN_PREFIX + "step")
+    @JobScope
+    public Step step() {
+        return stepBuilderFactory.get(BEAN_PREFIX + "step")
+                .<Pay, String>chunk(chunkSize)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean
+    public JpaPagingItemReader<Pay> reader() {
+        return new JpaPagingItemReaderBuilder<Pay>()
+                .name(BEAN_PREFIX+"reader")
+                .entityManagerFactory(emf)
+                .pageSize(chunkSize)
+                .queryString("SELECT t FROM Pay t")
+                .build();
+    }
+
+    @Bean
+    public ItemProcessor<Pay, String> processor() {
+        return pay -> {
+            return pay.getTxName();
+        };
+    }
+
+    private ItemWriter<String> writer() {
+        return items -> {
+            for (String item : items) {
+                log.info("pay getTxName={}", item);
+            }
+        };
+    }
+}
