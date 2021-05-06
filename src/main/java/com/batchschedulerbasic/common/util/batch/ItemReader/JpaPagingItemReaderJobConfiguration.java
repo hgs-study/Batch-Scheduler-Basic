a721e1450b7798af329b0dd9d0f9a4ca9036b6ef
hgs-study <hgstudy_@naver.com>
2021-05-06 23:10:27 +0900

    [Batch] ItemWriter (JpaPagingItemWriter)

--- src/main/java/com/batchschedulerbasic/common/util/batch/ItemReader/JpaPagingItemReaderJobConfiguration.java
+++ src/main/java/com/batchschedulerbasic/common/util/batch/ItemReader/JpaPagingItemReaderJobConfiguration.java
@@ -1,5 +1,6 @@
-package com.batchschedulerbasic.common.util.batch;
+package com.batchschedulerbasic.common.util.batch.ItemReader;
 
+import com.batchschedulerbasic.common.util.batch.Pay;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
